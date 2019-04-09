900218ef7406b09bbc01df6a15c41409c32865fb
Jiawa Di <zjjs_smile@163.com>
2019-04-10 02:06:06 +0800

    upload

---
+++ src/todo/header.vue
@@ -0,0 +1,18 @@
+<template>
+  <header class="main-header">
+    <h1>TodoList</h1>
+  </header>
+</template>
+
+<style lang="stylus" scoped>
+.main-header{
+  text-align center
+  h1{
+    margin 20px
+    font-size 80px
+    color rgba(175, 10, 10, 0.4)
+    font-weight 100
+  }
+}
+</style>
+
